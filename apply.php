427242b08104fac9aced124ae3926d89b1cd2436
anobayaneh <109091357+anobayaneh@users.noreply.github.com>
2026-07-31 16:22:08 +0800

    Update apply.php

--- apply.php
+++ apply.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/protection.php';
+//require_once __DIR__ . '/protection.php';
 session_start();
 
 /* ------------------------------------------------------------------
